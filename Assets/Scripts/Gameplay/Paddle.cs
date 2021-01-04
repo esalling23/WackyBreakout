bc3625dab672144d63555c81e84ed0ba5bf5c16a
esalling23 <esalling23@gmail.com>
2021-01-03 19:52:33 -0500

    Step 5: Keep the Paddle in the Playfield

--- Assets/Scripts/Gameplay/Paddle.cs
+++ Assets/Scripts/Gameplay/Paddle.cs
@@ -10,23 +10,47 @@ public class Paddle : MonoBehaviour
     private Rigidbody2D body;
     private Vector3 position;
     private float horizontalInput;
-
+    private float halfWidth;
 
     // Start is called before the first frame update
     void Start()
     {
+
         body = GetComponent<Rigidbody2D>();
+
+        // Gets the width of the box collider component
+        halfWidth = GetComponent<BoxCollider2D>().size.x / 2;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //  Horizontal axis is right/left arrow or a + d keys
         horizontalInput = Input.GetAxis("Horizontal");
         if (horizontalInput != 0) 
         {
             position = transform.position;
             position.x += horizontalInput * ConfigurationUtils.PaddleMoveUnitsPerSecond * Time.fixedDeltaTime;
+
+            // Calculate clamped position to keep paddle on screen
+            position.x = CalculateClampedX(position.x);
+
+            // Move paddle
             body.MovePosition((Vector2)position);
         }
+    }
+
+    private float CalculateClampedX(float newXPosition)
+    {
+        if (newXPosition <= ScreenUtils.ScreenLeft + halfWidth)
+        {
+            return ScreenUtils.ScreenLeft + halfWidth;
+        }
+        else if (newXPosition >= ScreenUtils.ScreenRight - halfWidth)
+        {
+            return ScreenUtils.ScreenRight - halfWidth;
+        }
+
+        return newXPosition;
     }
 }
